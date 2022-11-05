b908b4baca5853115c4c46a8a73de82a73513a43
eggdp <pur9799@gmail.com>
2022-11-05 17:35:25 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Counter from "pages/Counter";
+import Main from "pages/Main";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// import Footer from "./components/commons/Footer"; // components 폴더만들고 넣기
+// import Header from "./components/commons/Header";
+
+//return 에는 최상단 태그 하나만 가능 ctrl alt l 자동정렬
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Main />} />
+        <Route path={"/counter"} element={<Counter />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/commons/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div style={{ backgroundColor: "yellow" }}>footer입니다.</div>;
+};
+
+export default Footer;
